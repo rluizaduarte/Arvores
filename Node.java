f9ca96181a02a61bb7d5e448116eccd390193737
rluizaduarte <riellyduarte75@gmail.com>
2026-04-08 20:22:22 -0300

    implementação do nó

---
+++ Node.java
@@ -0,0 +1,16 @@
+public class Node {
+    public int key;
+    // valor do no
+    public Node left;
+    // filho a esquerda
+    public Node right;
+    // filho a direita
+
+    // construtor
+    public Node(int key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+
+}
